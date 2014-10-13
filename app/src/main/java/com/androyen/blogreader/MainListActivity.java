958b74532c1daf0998923fc320ff7fa61eb5a450
Androyen <androyen@aol.com>
2014-10-13 11:28:37 -0500

    Add HttpURLConnection and put it in main UI thread

--- app/src/main/java/com/androyen/blogreader/MainListActivity.java
+++ app/src/main/java/com/androyen/blogreader/MainListActivity.java
@@ -2,29 +2,58 @@ package com.androyen.blogreader;
 
 import android.app.ListActivity;
 import android.content.res.Resources;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames;
+    protected String[] mBlogPostTitles;
+
+    public static final int NUMBER_OF_POSTS = 20;
+
+    public static final String TAG = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
+        //Get data from the web
+
+        try {
+            URL blogFeedURL = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+            HttpURLConnection connection = (HttpURLConnection) blogFeedURL.openConnection();
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+            Log.i(TAG, "Code: " + responseCode + "");
+
+        }
+        catch (MalformedURLException e) {
+            Log.e(TAG, "Exception caught", e);
+        }
+        catch (IOException e) {
+            Log.e(TAG, "IOException caught", e);
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Exception caught", e);
+        }
+
         //Initialize String array
         Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.android_names);
+        mBlogPostTitles = resources.getStringArray(R.array.android_names);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
+
+
 
 //        String message = getString(R.string.no_items);
 //        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
